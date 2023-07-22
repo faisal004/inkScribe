1dc32d32635417ba98b0bda5a3c3b9b108c53a02
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-07-22 21:04:08 +0530

    protected routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,37 +1,55 @@
-import Navbar from './components/Navbar'
-import Welcome from './components/Welcome'
-import Signup from './pages/Signup';
-import Login from './pages/Login';
-import './App.css'
+import Navbar from "./components/Navbar";
+import Welcome from "./components/Welcome";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Write from "./pages/Write";
+import "./App.css";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
- 
+  Navigate,
 } from "react-router-dom";
+import { useRecoilValue } from "recoil";
+import { userState } from "../Recoil/stateManagement";
 
 function App() {
+  const user = useRecoilValue(userState);
 
+  const PrivateRoute = ({ children }) => {
+    return user ? children : <Navigate to="/" />;
+  };
 
   return (
-
-
     <>
-    <Router>
-    <Navbar/>
-    <Routes>
-      <Route path="/" element={<Welcome/>}/>
-      <Route path="/Login" element={<Login/>}/>
-      <Route path="/Signup" element={<Signup/>}/>
-
-     
-    </Routes>
-     
-
-    </Router>
-    
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Signup" element={<Signup />} />
+         
+          <Route
+            path="/Home"
+            element={
+              <PrivateRoute>
+                <Home />
+              </PrivateRoute>
+            }
+          />
+             <Route
+            path="/Write"
+            element={
+              <PrivateRoute>
+                <Write />
+              </PrivateRoute>
+            }
+          />
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
