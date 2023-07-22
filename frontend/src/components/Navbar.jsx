49730ded1b3e07036fe6be4636a7bae1d9aec766
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-07-22 11:33:58 +0530

    no change

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 
+
 const Navbar = () => {
   const navigate = useNavigate();
 
