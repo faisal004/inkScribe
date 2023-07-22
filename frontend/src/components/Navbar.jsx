cba2071bcb306c6e52597417296253bc81e8a0a6
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-07-22 12:55:49 +0530

    refactor

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { userState } from "../../Recoil/stateManagement";
 
-import { useRecoilState, useSetRecoilState } from "recoil";
+import { useRecoilState} from "recoil";
 
 const Navbar = () => {
   const navigate = useNavigate();
