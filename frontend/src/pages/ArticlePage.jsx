a06875c83828fa915d15b4f36186d974de11fb64
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-07-23 13:31:52 +0530

    config file added

--- frontend/src/pages/ArticlePage.jsx
+++ frontend/src/pages/ArticlePage.jsx
@@ -1,30 +1,35 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { BASE_URL } from "../../config";
+import {
+  AiOutlineHeart,
+  AiFillSave,
+  AiFillHeart,
+  AiOutlineSave,
+} from "react-icons/ai";
 
 const ArticlePage = () => {
   const [post, setPost] = useState(null);
+  const [heartFilled, setHeartFilled] = useState(false);
+  const [saveFilled, setSaveFilled] = useState(false);
   const { PostId } = useParams();
-  console.log(PostId)
 
   useEffect(() => {
     const fetchPost = async () => {
       try {
-       
         const token = localStorage.getItem("token");
-        console.log(token)
-        const response = await fetch('http://localhost:3000/PostBlog/'+PostId, {
+        console.log(token);
+        const response = await fetch(`${BASE_URL}/PostBlog/` + PostId, {
           method: "GET",
           headers: {
             "Content-Type": "application/json",
             Authorization: `Bearer ${token}`,
           },
-          
         });
 
         const data = await response.json();
-        console.log(data)
+
         setPost(data.post);
-        
       } catch (error) {
         console.error("Error fetching post:", error);
       }
@@ -36,6 +41,13 @@ const ArticlePage = () => {
   if (!post) {
     return <div>Loading...</div>;
   }
+
+  const handleHeartClick = () => {
+    setHeartFilled((prevState) => !prevState);
+  };
+  const handleSaveClick = () => {
+    setSaveFilled((prevState) => !prevState);
+  };
 
   return (
     <div className="min-h-screen bg-slate-50">
@@ -52,15 +64,26 @@ const ArticlePage = () => {
             alt="Profile"
           />
         </div>
-        <h2 className="text-xl font-medium">{post.creator.username}</h2>
-        <p className="text-slate-500">{Math.ceil(post.mainContent.split(" ").length / 200)} min read</p>
+        <h2 className="text-xl font-medium p-2">{post.creator.username}</h2>
+        <p className="text-slate-500 p-2">
+          {Math.ceil(post.mainContent.split(" ").length / 200)} min read
+        </p>
+        <div className="flex flex-row space-x-2 p-2 border-t-2 border-b-2 border-black">
+          <div onClick={handleHeartClick}>
+            {heartFilled ? <AiFillHeart color="red" /> : <AiOutlineHeart />}
+          </div>
+          <div onClick={handleSaveClick}>
+            {saveFilled?<AiFillSave color="black" />:<AiOutlineSave/>}
+            
+          </div>
+        </div>
       </main>
 
       <section className="bg-slate-50 py-8 px-4 sm:px-6 lg:px-8">
-        <div className="max-w-3xl mx-auto"
-        dangerouslySetInnerHTML={{ __html: post.mainContent }}/>
-         
-      
+        <div
+          className="max-w-3xl mx-auto"
+          dangerouslySetInnerHTML={{ __html: post.mainContent }}
+        />
       </section>
     </div>
   );
