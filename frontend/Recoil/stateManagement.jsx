0c060d0e90fb11fbea8d8622091863c737586ff0
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-07-24 20:40:34 +0530

    optimisation

--- frontend/Recoil/stateManagement.jsx
+++ frontend/Recoil/stateManagement.jsx
@@ -34,31 +34,8 @@ export const loginAPI = selector({
     return data;
   },
 });
-export const signupState = atom({
-  key: "signupState",
-  default: {
-    username: "",
-    email: "",
-    password: "",
-  },
-});
-export const signupAPI = selector({
-  key: "signupAPI",
-  get: async ({ get }) => {
-    const signupData = get(signupState);
 
-    const response = await fetch("http://localhost:3000/user/Signup", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(signupData),
-    });
 
-    const data = await response.json();
-    return data;
-  },
-});
 export const editorState = atom({
   key: "editorState",
   default: {
