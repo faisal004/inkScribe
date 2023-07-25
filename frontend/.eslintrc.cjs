7fbe8169c2a2510285b4df705e9ab414cf4a4c2e
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-07-25 09:33:46 +0530

    some config

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -14,7 +14,9 @@ module.exports = {
   rules: {
     'react-refresh/only-export-components': [
       'warn',
+      
       { allowConstantExport: true },
     ],
+    "react/prop-types": "off"
   },
 }
