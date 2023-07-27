c512f4f6cc8d65f115f63cce0e554f63f343d2db
FAISAL HUSAIN <88244542+faisal004@users.noreply.github.com>
2023-07-27 11:05:00 +0530

    port

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ import bodyParser from "body-parser";
 import Postrouter from "./routes/postRoutes.js"
 
 const app = express();
-const port =process.env.PORT|| 3000;
+const port =process.env.PORT;
 connectDB();
 app.use(cors());
 app.use(bodyParser.json());
